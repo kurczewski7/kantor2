31aa9329e6cdb449ddaac33b31bcd251ee5b42d5
kurczewski7 <slawomir.kurczewski@gmail.com>
2016-11-20 00:01:01 +0100

    Po wprowadzeniu przeliczeń walut

--- Kantor2/ExchangeViewController.swift
+++ Kantor2/ExchangeViewController.swift
@@ -16,17 +16,36 @@ class ExchangeViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var hideUserInputButton: UIBarButtonItem!
     @IBOutlet weak var exchangeLab: UILabel!
     
+    var result:(buy:Float, sell:Float) = (0.0,0.0)
+    
     
     @IBAction func actionHideUserInputs(_ sender: AnyObject) {
         userAmountTextField.resignFirstResponder()
         navigationItem.rightBarButtonItems=nil
     }
     
+    @IBAction func actionUserAmountChanged(_ sender: AnyObject) {
+        result.buy=Float(userAmountTextField.text!)!*3
+        result.sell=Float(userAmountTextField.text!)!*4
+    }
     
+    @IBAction func actionUpdateInterface() {
+        var displayResult:Float=0.0
+        if(segmentedControl.selectedSegmentIndex==0)
+        {
+            displayResult=result.sell
+        }
+        else
+        {
+            displayResult=result.buy
+        }
+        exchangeLab.text="\(displayResult)"
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         userAmountTextField.text="100"
+        actionUserAmountChanged(userAmountTextField)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
