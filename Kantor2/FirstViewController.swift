d470a2a8d049eb8d40db8da8f1eb3f61417f4f92
Slawek Kurczewski <slawek@Mac-mini-Slawek.local>
2016-11-19 16:16:42 +0100

    Initial Commit

---
+++ Kantor2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Kantor2
+//
+//  Created by Slawek Kurczewski on 19.11.2016.
+//  Copyright © 2016 Slawomir Kurczewski. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
