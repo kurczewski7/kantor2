887ab1c535a515607ad888509f56ecd0caf0d130
kurczewski7 <slawomir.kurczewski@gmail.com>
2016-11-20 10:38:29 +0100

    Po dołączeniu tablicy walut

--- Kantor2/source/Kantor.swift
+++ Kantor2/source/Kantor.swift
@@ -9,6 +9,29 @@
 import UIKit
 
 class Kantor: NSObject {
+    override init()
+    {
+        let path = Bundle.main.path(forResource: "currencies", ofType:"plist")
+            if path != nil
+            {
+                print("path\(path)")
+                if let list=NSArray(contentsOfFile: path!) as? Array<AnyObject>
+                {
+                    for item in list
+                    {   print("item:\(item)") }
+                }
+            }
+            else
+            {
+                print("Currencies file not found")
+                abort()
+            }
+    }
+        
+        
+        
+        
+        //NSBundle.mainBundle().pathForResource("currencies", ofType:"plist"?)
     func exchange(amount:Float, currencyCode:String) -> (buy:Float, sell:Float) {
         
         let buy = amount*4.21
