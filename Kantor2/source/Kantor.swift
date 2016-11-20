02bd4c9b968ea36a0ae20013cf17e657cd4b46b0
kurczewski7 <slawomir.kurczewski@gmail.com>
2016-11-20 11:46:23 +0100

    Po podłączeniu słownika walut

--- Kantor2/source/Kantor.swift
+++ Kantor2/source/Kantor.swift
@@ -8,7 +8,22 @@
 
 import UIKit
 
+struct Currency {
+    var code:String
+    var name : String
+    var buy : Float
+    var sell: Float
+    var conversion: Float
+    
+
+}
+
+
+
 class Kantor: NSObject {
+    var currencies: [String : Currency]=[:]
+    
+    
     override init()
     {
         let path = Bundle.main.path(forResource: "currencies", ofType:"plist")
@@ -18,7 +33,20 @@ class Kantor: NSObject {
                 if let list=NSArray(contentsOfFile: path!) as? Array<AnyObject>
                 {
                     for item in list
-                    {   print("item:\(item)") }
+                    {   print("item:\(item)")
+                        
+                        let name = item["name"] as! String
+                        let code = item["code"] as! String
+                        let buy = item["buy"] as! Float
+                        let sell = item["sell"] as! Float
+                        let conversion = item["conversion"] as! Float
+                        
+                        let curr = Currency(code: code, name: name, buy: buy, sell: sell, conversion: conversion)
+                        
+                        currencies.updateValue(curr, forKey: code)
+                    
+                    
+                    }
                 }
             }
             else
@@ -33,10 +61,14 @@ class Kantor: NSObject {
         
         //NSBundle.mainBundle().pathForResource("currencies", ofType:"plist"?)
     func exchange(amount:Float, currencyCode:String) -> (buy:Float, sell:Float) {
-        
-        let buy = amount*4.21
-        let sell = amount*3.58
-    
-        return (buy, sell)
+        if let selected = currencies[currencyCode]
+        {
+            let buy = amount*selected.buy
+            let sell = amount*selected.sell
+            return (buy, sell)
+
+        }
+        print("Currency data is missing for code \(currencyCode)")
+        return (0.0,0.0)
     }
 }
